dfe884f4413adf17539a347a0dbe03c753dad9bd
kornishontv <72425258+kornishontv@users.noreply.github.com>
2023-04-09 16:33:44 +0300

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,10 @@
+sudo apt-get install python3
+pip install pyautogui
+pip install moderngl
+pip install moderngl_window
+pip install screeninfo
+git clone https://github.com/kornishontv/RandomOS
+sudo apt install openbox
+mv ./RandomOS ~/Документы
+cd Документы
+mv ./RandomOS/RandomOS_Desktop/autostart.sh ~/.config/openbox/
