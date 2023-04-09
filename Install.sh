08ace733ab89ad4730037e6244724d0426a967c2
kornishontv <72425258+kornishontv@users.noreply.github.com>
2023-04-09 16:47:07 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -5,6 +5,5 @@ pip install moderngl_window
 pip install screeninfo
 git clone https://github.com/kornishontv/RandomOS
 sudo apt install openbox
-mv ~/RandomOS ~/Документы
-cd ~/Документы
-mv ~/RandomOS/RandomOS_Desktop/autostart.sh ~/.config/openbox/
+mv ./RandomOS ~/Документы
+mv ~/Документы/RandomOS/RandomOS_Desktop/autostart.sh ~/.config/openbox/
