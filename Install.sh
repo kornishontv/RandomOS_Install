508e3e6dd56951b3f79fb3fb6d562dadcb1da56f
kornishontv <72425258+kornishontv@users.noreply.github.com>
2023-04-13 19:34:17 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,10 +1,12 @@
 sudo apt-get install python3
+sudo apt install python3-pip
 pip install pyautogui
 pip install moderngl
 pip install moderngl_window
 pip install screeninfo
 git clone https://github.com/kornishontv/RandomOS
 sudo apt install openbox
+sudo apt-get install python3-tk python3-dev
 mv ./RandomOS ~/Документы
 mv ~/Документы/RandomOS/RandomOS_Desktop/autostart.sh ~/.config/openbox/
 chmod u+x ~/.config/openbox/autostart.sh
